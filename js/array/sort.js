cf15abfaf02ec6b86d5f3e9efbdbc9af519e00a6
dev-mahir <mdmahir.ds@gmail.com>
2023-05-21 06:22:00 +0600

    update

--- js/array/sort.js
+++ js/array/sort.js
@@ -7,4 +7,10 @@ const fruits = ["Banana", "Orange", "Apple", "orange", "Mango", "banana"];
 // console.log(reverse);
 
 
+// but in numeric order , we use compare function 
+const num = [1, 21, 45, 65, 85, 74, 14, 3, 65];
+
+
+
+console.log(num.sort((a, b) => a - b));
 
